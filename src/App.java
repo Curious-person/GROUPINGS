3689492f0759a3e8af390fc3064b8c8ce83c1a1a
John Kevin Abgao <111432788+Curious-person@users.noreply.github.com>
2023-06-21 13:57:56 +0800

    added app icon on scenes and alert button

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
@@ -49,8 +50,17 @@ public class App extends Application {
 
         Alert alert = new Alert(AlertType.CONFIRMATION  );
         alert.setTitle("Exit");
-        alert.setHeaderText("You are about to leave JAACK.");
-        alert.setContentText("Do you wish to stop shopping?: ");
+        alert.setHeaderText("You are about to leave JACCK.");
+        alert.setContentText("Do you wish to stop shopping? ");
+  
+            // Load the app icon
+        Image appIcon = new Image("images/JACCK-removebg-preview.png");
+        ImageView imageView = new ImageView(appIcon);
+        imageView.setFitWidth(48); // Set the desired width of the icon
+        imageView.setFitHeight(48); // Set the desired height of the icon
+
+        // Set the app icon as the graphic of the alert dialog
+        alert.setGraphic(imageView);
 
         if (alert.showAndWait().get() == ButtonType.OK) {
             System.out.println("You have successfully logged out.");
